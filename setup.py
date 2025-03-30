ab97bf893cdd958a3380647ff82bba980e1eaeb7
parasite-e <fardinemonbd@gmail.com>
2025-03-30 14:53:51 +0600

    project structure build

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Emon Hossain",
+    author_email="emonhossianbd@gmail.com",
+    packages=find_packages()
+)
